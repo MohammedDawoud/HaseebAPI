a1fdb68e7e828841845dcbd036129d2d4f21a145
Mohammed Dawoud <mohammeddawoud66@gmail.com>
2025-04-29 17:02:16 +0300

    aaa edit

--- TaamerProject.API/Controllers/VoucherController.cs
+++ TaamerProject.API/Controllers/VoucherController.cs
@@ -1343,7 +1343,7 @@ namespace TaamerProject.API.Controllers
                 var Msg = new GeneralMessage { StatusCode = HttpStatusCode.BadRequest, ReasonPhrase = "لا يمكن حفظ تاريخ في سنة مالية مختلفة" };
                 return Ok(Msg);
             }
-            var result = _voucherService.SaveInvoiceForServicesNoti(voucher, _globalshared.UserId_G, _globalshared.BranchId_G, _globalshared.YearId_G);
+            var result = _voucherService.SaveInvoiceForServicesNoti(voucher, _globalshared.UserId_G, _globalshared.BranchId_G, _globalshared.YearId_G, Con ?? "");
             //var syssetting = _systemSettingsService.GetSystemSettingsByBranchId(BranchId);
             //if (syssetting.UploadInvZatca == true)
             //{
@@ -1489,7 +1489,7 @@ namespace TaamerProject.API.Controllers
                 var Msg = new GeneralMessage { StatusCode = HttpStatusCode.BadRequest, ReasonPhrase = "لا يمكن حفظ تاريخ في سنة مالية مختلفة" };
                 return Ok(Msg);
             }
-            var result = _voucherService.SavePurchaseForServicesNotiDepit(voucher, _globalshared.UserId_G, _globalshared.BranchId_G, _globalshared.YearId_G);
+            var result = _voucherService.SavePurchaseForServicesNotiDepit(voucher, _globalshared.UserId_G, _globalshared.BranchId_G, _globalshared.YearId_G, Con ?? "");
             return Ok(new { result.StatusCode, result.ReasonPhrase, result.ReturnedParm });
         }
         [HttpPost("SaveandPostPurchaseForServices")]
@@ -1703,6 +1703,15 @@ namespace TaamerProject.API.Controllers
             var Value = _voucherService.GenerateVoucherNumber(Type, _globalshared.BranchId_G, _globalshared.YearId_G).Result;
             var NewValue = string.Format("{0:000000}", Value);
             var generatevalue = new GeneralMessage { StatusCode = HttpStatusCode.OK, ReasonPhrase = NewValue };
+            return Ok(generatevalue);
+        }
+        [HttpGet("GenerateVoucherNumberNew")]
+
+        public ActionResult GenerateVoucherNumberNew(int Type)
+        {
+            HttpContext httpContext = HttpContext; _globalshared = new GlobalShared(httpContext);
+            var result = _voucherService.GenerateVoucherNumberNewPro(Type, _globalshared.BranchId_G, _globalshared.YearId_G, Type, Con ?? "").Result;
+            var generatevalue = new GeneralMessage { StatusCode = HttpStatusCode.OK, ReasonPhrase = result };
             return Ok(generatevalue);
         }
         [HttpGet("GenerateVoucherNumberOpening")]
